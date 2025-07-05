bce7671fdb10b7e125bbf87fe3d75c3514cb5eb5
N4no57 <b.rego.oliveira@gmail.com>
2025-07-06 00:42:17 +0100

    implemented allocate_cluster_chain() in fat_table.py

--- src/fat_table.py
+++ src/fat_table.py
@@ -6,6 +6,25 @@ for i in range(DATA_SECTOR_START):
 
 def allocate_cluster_chain(count: int) -> list:
     """Allocate a chain of `count` clusters and return their indices in order."""
+    cluster_chain = []
+    fat_index = 0
+    while count > 0:
+        if FAT[fat_index] != 0x00:
+            fat_index+=1
+            continue
+
+        cluster_chain.append(fat_index)
+        fat_index+=1
+        count-=1
+
+    for i in range(len(cluster_chain)):
+        if i == (len(cluster_chain)-1):
+            FAT[cluster_chain[i]] = 0xFF
+            break
+
+        FAT[cluster_chain[i]] = cluster_chain[i+1]
+
+    return cluster_chain
 
 def free_cluster_chain(start_cluster: int) -> None:
     """Free all clusters in the chain starting at `start_cluster`."""
