4849c7813ca46746c73fcbf1eadeb67616fd45df
N4no57 <b.rego.oliveira@gmail.com>
2025-07-06 00:02:02 +0100

    created helper functions in directoryEntry.py

--- src/directoryEntry.py
+++ src/directoryEntry.py
@@ -35,4 +35,34 @@ class DirectoryEntry:
         return self.data[28] + (self.data[29] << 8) + (self.data[30] << 16) + (self.data[31] << 24)
 
     def __repr__(self):
-        return f"<DirectoryEntry '{self.filename}.{self.extension}' size={self.file_size} cluster={self.first_cluster}>"
+        if self.attributes != 0x10:
+            return f"<DirectoryEntry '{self.filename}.{self.extension}' size={self.file_size} cluster={self.first_cluster}>"
+        else:
+            return f"<DirectoryEntry '{self.filename}' cluster={self.first_cluster}>"
+
+##################################################
+### HELPER FUNCTIONS
+###################################################
+
+def create_dot_entry(cluster):
+    entry = [0] * 32
+    # Filename '.' padded to 8 bytes with spaces
+    entry[0] = ord('.')
+    for i in range(1, 11):
+        entry[i] = 0x20
+    entry[11] = 0x10 # directory attribute
+    entry[26] = cluster & 0xFF
+    entry[27] = (cluster >> 8) & 0xFF
+    return entry
+
+def create_dotdot_entry(parent_cluster):
+    entry = [0] * 32
+    # Filename '..' padded to 8 bytes with spaces
+    entry[0] = ord('.')
+    entry[1] = ord('.')
+    for i in range(2, 11):
+        entry[i] = 0x20
+    entry[11] = 0x10
+    entry[26] = parent_cluster & 0xFF
+    entry[27] = (parent_cluster >> 8) & 0xFF
+    return entry
