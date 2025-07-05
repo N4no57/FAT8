87a87af7eaee06f4d8eec22eb1be97e106476e22
N4no57 <b.rego.oliveira@gmail.com>
2025-07-06 00:21:23 +0100

    finished write_sector() in disk.py

--- src/disk.py
+++ src/disk.py
@@ -8,3 +8,6 @@ def read_sector(sector_number: int) ->bytes:
 def write_sector(sector_number: int, data: bytes) -> None:
     if len(data) != SECTOR_SIZE:
         raise ValueError('Data must be of size {}'.format(SECTOR_SIZE))
+    with open(IMAGE_PATH, 'r+b') as image:
+        image.seek(sector_number*SECTOR_SIZE)
+        image.write(data)
