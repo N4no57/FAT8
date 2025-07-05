25aed32c180b0fa53b684da38cd1be48a6328c4d
N4no57 <b.rego.oliveira@gmail.com>
2025-07-06 00:18:49 +0100

    created disk.py for abstracted I/O

---
+++ src/disk.py
@@ -0,0 +1,10 @@
+from utils import SECTOR_SIZE, IMAGE_PATH
+
+def read_sector(sector_number: int) ->bytes:
+    with open(IMAGE_PATH, 'rb') as image:
+        image.seek(sector_number*SECTOR_SIZE)
+        return image.read(SECTOR_SIZE)
+
+def write_sector(sector_number: int, data: bytes) -> None:
+    if len(data) != SECTOR_SIZE:
+        raise ValueError('Data must be of size {}'.format(SECTOR_SIZE))
