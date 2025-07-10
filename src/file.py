30436958e5f227468a16754db4c918124cf0d034
NanoDev <b.rego.oliveira@gmail.com>
2025-07-10 22:12:05 +0100

    extracted block of code to follow DRY

--- src/file.py
+++ src/file.py
@@ -42,6 +42,18 @@ def create_file(FAT: list[int], dir_cluster: int, name: str, extension: str, dat
 def delete_file(FAT: list[int], entry: DirectoryEntry) -> bool:
     """Remove file and free its clusters."""
 
+def write_to_file_location(cluster_chain, data):
+    chain_num = 0
+    for i in range(0, len(data), 512):
+        partition = data[i:i + 512]
+
+        if len(partition) != 512:
+            for index in range(512 - len(partition)):
+                partition += b'\x00'
+
+        write_sector(cluster_chain[chain_num], partition)
+        chain_num += 1
+
 def find_duplicate_file(FAT: list[int], dir_cluster: int, name: str, extension: str) -> bool:
     """Find duplicate files."""
     file_entry = find_entry(FAT, name, dir_cluster)
