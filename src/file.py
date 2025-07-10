0ae419dd47ca2729e0ca4b8b12f195277004d4c2
NanoDev <b.rego.oliveira@gmail.com>
2025-07-10 18:20:55 +0100

    added checking for duplicate files

--- src/file.py
+++ src/file.py
@@ -1,14 +1,20 @@
-from src.directory import create_entry
+from src.directory import create_entry, find_entry
 from src.disk import write_sector
 from src.fat_table import get_cluster_chain
 from src.fs import DirectoryEntry
 
-def read_file(FAT: list[int],entry: DirectoryEntry) -> bytes:
+def read_file(FAT: list[int], file) -> bytes:
     """Read a file's contents by following its cluster chain."""
+
 
 def write_file(FAT: list[int], dir_cluster: int, name: str, extension: str, data: bytes) -> bool:
     """Write a new file into a given directory cluster."""
     size = (len(data)//512)+1
+    file_entry = find_entry(FAT, name, dir_cluster)
+    if file_entry is not None:
+        print("file already exists")
+        return False
+
     file_entry = create_entry(FAT, name, extension,
                               parent_cluster=dir_cluster, size=size)
     cluster_chain = get_cluster_chain(FAT, file_entry.first_cluster)
